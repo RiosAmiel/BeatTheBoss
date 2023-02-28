f275bf1894b74bb9d2ba320a64b26462aed8cab5
Amiel <amiel.rios@gmail.com>
2023-02-28 17:00:34 +0800

    updated and changed game in btb

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { AfterViewInit, ElementRef, OnInit, ViewChild, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit, AfterViewInit{
+  ngOnInit() {
+    this.shuffleArray(this.label1);
+  }
+  date: any;
+  now: any;
+  targetDate: any = new Date(2022, 12, 9);
+  targetTime: any = this.targetDate.getTime();
+  difference!: number;
+  months: Array<string> = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  currentTime: any = `${
+    this.months[this.targetDate.getMonth()]
+  } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
+
+  @ViewChild('days', { static: true }) days!: ElementRef;
+  @ViewChild('hours', { static: true }) hours!: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes!: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds!: ElementRef;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+      !isNaN(this.days.nativeElement.innerText)
+        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
+        : (this.days.nativeElement.innerHTML = `<img src="https://i.gifer.com/VAyR.gif" />`);
+    }, 1000);
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+    this.days.nativeElement.innerText = Math.floor(this.difference);
+    this.hours.nativeElement.innerText = 23 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
+  title = 'BeatTheBoss';
+
+  label1: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+   'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+
+  shuffledLabel: string[] = this.shuffleArray([this.label1]);
+
+  shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+}
